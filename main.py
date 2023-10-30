6b1c245228e0f0770e84cd122aed6912ba8fbfd7
Abhishek kumar <sahniak56@gmail.com>
2023-10-30 15:09:36 +0530

    jb

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world")
+import requests
+
+url = "https://github.com/bratsche/pango/raw/4de30e5500eaeb49f4bf0b7a07f718e149a2ed5e/pango/glyphstring.c"
+response = requests.get(url)
+
+if response.status_code == 200:
+    with open("glyphstring.c", "wb") as file:
+        file.write(response.content)
+    print("File downloaded and saved as glyphstring.c")
+else:
+    print("Failed to download the file.")
